624e46f3ce06515793c24ded56d324bc5df97228
Mai Tran <mai.t.tran1743@gmail.com>
2022-12-04 18:30:10 -0500

    Change function headers

--- src/YelpDataset.h
+++ src/YelpDataset.h
@@ -9,8 +9,8 @@ private:
     double ufLng = -82.34944;
     double userLat;
     double userLng;
-
-    void loadData(std::vector<YelpBusiness>& data, int lat, int lng);
+    
+    void loadData(std::vector<YelpBusiness>& data);
 
     void merge(std::vector<YelpBusiness>& vec, int low, int mid, int high, int sortKind, bool descending);
     void mergeSort(std::vector<YelpBusiness>& vec, int low, int high, int sortKind, bool descending);
@@ -21,9 +21,8 @@ private:
     std::vector<YelpBusiness> data;
 
 public:
-    YelpDataset();
-    YelpDataset(int userLat, int userLng);
-    void sortTimer(int type, int sortKind, bool descending);
+    YelpDataset(double userLat, double userLng);
+    std::vector<YelpBusiness> sortTimer(int type, int sortKind, bool descending);
     std::vector<YelpBusiness> quickSortCaller(std::vector<YelpBusiness> vec, int sortKind, bool descending);
     std::vector<YelpBusiness> mergeSortCaller(std::vector<YelpBusiness> vec, int sortKind, bool descending);
 
