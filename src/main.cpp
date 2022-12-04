07ac79cfb51d745e7c88ae8ec8090c33857519f2
burschc <117326218+burschc@users.noreply.github.com>
2022-12-03 22:53:09 -0500

    update main

--- src/main.cpp
+++ src/main.cpp
@@ -2,4 +2,13 @@
 
 int main() {
     YelpDataset data;
+
+    data.sortTimer(2, 1, false); 
+    //synatx:
+    //first slot: 1 = mergesort, 2 = quicksort
+    //second slot: 1 = stars, 2 = ratings, 3 = distance
+    //third: "is descending": false = ordered logically, highest stars, rating, lowest distance | true = illogically, lowest stars and ratings, highest distance
+
+    int yeah; //this is just to make it so i could type without closing the window
+    std::cin >> yeah;
 }
