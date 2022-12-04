0f30478c02166d7c9621938452e80da1eab81aac
burschc <117326218+burschc@users.noreply.github.com>
2022-12-03 22:50:59 -0500

    distance function

--- src/YelpBusiness.cpp
+++ src/YelpBusiness.cpp
@@ -1,5 +1,6 @@
 #include "YelpBusiness.h"
 #include <iomanip>
+#include <cmath>
 
 // Prints information about the business.
 void YelpBusiness::print() {
@@ -8,6 +9,7 @@ void YelpBusiness::print() {
     std::cout << "Stars: " << stars << std::endl;
     std::cout << "Reviews: " << reviewCount << std::endl;
     std::cout << "Weighted Rating: " << std::fixed << std::setprecision(2) << rating << std::endl;
+    std::cout << "Distance: " << std::fixed << std::setprecision(2) << distance << std::endl;
     std::cout << "Hours: " << std::endl;
 
     std::vector<std::string> days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
@@ -26,4 +28,10 @@ void YelpBusiness::makeRating(double stars, int reviewCount) {
     
     // Bayesian average calculation
     this->rating = ((avgRating * numReviewsNeeded) + (stars * reviewCount)) / (numReviewsNeeded + reviewCount);
+}
+
+void YelpBusiness::calculateDistance(double lat, double lng) { //UF is at 29.64833 -82.34944 //replace this with the better calculation
+    double locLat = this->latitude;
+    double locLng = this->longitude;
+    this->distance = std::sqrt(std::pow(lat - locLat, 2) + std::pow(lng - locLng, 2));
 }
