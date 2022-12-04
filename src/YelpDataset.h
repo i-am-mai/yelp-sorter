bf7487e2e10e76cdcd267d85a34e7923766ec14c
burschc <117326218+burschc@users.noreply.github.com>
2022-12-03 22:52:56 -0500

    add the sorts to this file

--- src/YelpDataset.h
+++ src/YelpDataset.h
@@ -2,9 +2,29 @@
 
 class YelpDataset {
 private:
+    //std::vector<YelpBusiness> data;
+    //void loadData(std::vector<YelpBusiness>& data);
+
+    double ufLat = 29.64833;
+    double ufLng = -82.34944;
+    double userLat;
+    double userLng;
+
+    void loadData(std::vector<YelpBusiness>& data, int lat, int lng);
+
+    void merge(std::vector<YelpBusiness>& vec, int low, int mid, int high, int sortKind, bool descending);
+    void mergeSort(std::vector<YelpBusiness>& vec, int low, int high, int sortKind, bool descending);
+
+    int partition(std::vector<YelpBusiness>& vec, int low, int high, double piv, int sortKind, bool descending);
+    void quickSort(std::vector<YelpBusiness>& vec, int low, int high, int sortKind, bool descending);
+
     std::vector<YelpBusiness> data;
-    void loadData(std::vector<YelpBusiness>& data);
-    
+
 public:
     YelpDataset();
+    YelpDataset(int userLat, int userLng);
+    void sortTimer(int type, int sortKind, bool descending);
+    std::vector<YelpBusiness> quickSortCaller(std::vector<YelpBusiness> vec, int sortKind, bool descending);
+    std::vector<YelpBusiness> mergeSortCaller(std::vector<YelpBusiness> vec, int sortKind, bool descending);
+
 };
